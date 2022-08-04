1a379517459907378718f0234b9ddc34b6aaea93
midhunvnadh <midhun.vnadh5732@gmail.com>
2022-08-04 21:02:29 +0530

    Use firebase

---
+++ build.sh
@@ -0,0 +1,10 @@
+pip3 install -r requirements.txt
+now=$(date)
+python3 runner.py 2>&1 | tee build.log
+
+rm -rf build/*.json build/*.log build/*.date
+
+cp providers.json build/
+cp servers.json build/
+cp build.log build/
+echo "$now" > build/build.date
