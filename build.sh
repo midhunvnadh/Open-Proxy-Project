ec0fcfa310475b935b0f0c6254ad136e37895eb8
Midhun V Nadh <32438353+midhunvnadh@users.noreply.github.com>
2022-08-06 16:16:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 pip3 install -r requirements.txt
-now=$(date)
 python3 runner.py 2>&1 | tee build.log
 
 rm -rf build/*.json build/*.log build/*.date
@@ -7,5 +6,7 @@ rm -rf build/*.json build/*.log build/*.date
 cp providers.json build/
 cp servers.json build/
 cp build.log build/
+
+now=$(date)
 echo "$now" > build/build.date
 echo "Build complete"
