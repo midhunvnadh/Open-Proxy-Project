a35f3b0268fd51079abbad468611a57624db9c92
midhunvnadh <midhun.vnadh5732@gmail.com>
2022-08-14 20:30:32 +0530

    Websockets for live log

--- api/index.js
+++ api/index.js
@@ -1,9 +1,43 @@
 import 'dotenv/config';
 import express from 'express';
 import { MongoClient } from "mongodb";
+import { Server } from "socket.io";
+import fs from 'fs';
+
 const app = express();
+import http from 'http';
 
 const client = new MongoClient(process.env.MONGO_CONN_URL);
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, { cors: { origin: "*" } })
+
+
+const logfile = "/var/log/oproxy_runner.log"
+try {
+    fs.watchFile(logfile,
+        {
+            persistent: true,
+            interval: 200
+        },
+        (curr, prev) => {
+            var emit = ""
+            fs.readFile(logfile, 'utf-8', (err, data) => {
+                if (err) throw err;
+                let lines = data.trim().split("\n")
+                var emit = (lines[lines.length - 1])
+                io.emit('log', { line: emit });
+                console.log("Emitted: ", emit)
+            })
+        }
+    );
+}
+catch (e) {
+    console.log(e);
+}
+
 
 app.get('/', (req, res) => {
     const host = req.headers.host
@@ -41,6 +75,6 @@ app.get('/servers', async (req, res) => {
     }
 })
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running on port 3000');
+server.listen(process.env.PORT || 8080, () => {
+    console.log('Server is running on port 8080');
 })
